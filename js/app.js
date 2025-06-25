4e5020a293c052f7702a17287086422552b80042
shividoge <143344941+shividoge@users.noreply.github.com>
2025-06-25 01:38:39 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -225,62 +225,72 @@ try {
 /*      Contact Js       */
 /*************************/
 
-try {
-    function validateForm() {
-        var name = document.forms["myForm"]["name"].value;
-        var email = document.forms["myForm"]["email"].value;
-        var subject = document.forms["myForm"]["subject"].value;
-        var comments = document.forms["myForm"]["comments"].value;
-        document.getElementById("error-msg").style.opacity = 0;
-        document.getElementById('error-msg').innerHTML = "";
-        if (name == "" || name == null) {
-            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
-            fadeIn();
-            return false;
-        }
-        if (email == "" || email == null) {
-            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
-            fadeIn();
-            return false;
-        }
-        if (subject == "" || subject == null) {
-            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
-            fadeIn();
-            return false;
-        }
-        if (comments == "" || comments == null) {
-            document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
-            fadeIn();
-            return false;
-        }
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("simple-msg").innerHTML = this.responseText;
-                document.forms["myForm"]["name"].value = "";
-                document.forms["myForm"]["email"].value = "";
-                document.forms["myForm"]["subject"].value = "";
-                document.forms["myForm"]["comments"].value = "";
-            }
-        };
-        xhttp.open("POST", "php/contact.php", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+function validateForm() {
+    var name = document.forms["myForm"]["name"].value.trim();
+    var email = document.forms["myForm"]["email"].value.trim();
+    var subject = document.forms["myForm"]["subject"].value.trim();
+    var comments = document.forms["myForm"]["comments"].value.trim();
+
+    var errorMsgElem = document.getElementById('error-msg');
+    var simpleMsgElem = document.getElementById('simple-msg');
+    errorMsgElem.style.opacity = 0;
+    errorMsgElem.innerHTML = "";
+    simpleMsgElem.innerHTML = "";
+
+    if (name === "") {
+        errorMsgElem.innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
+        fadeIn();
+        return false;
+    }
+    if (email === "") {
+        errorMsgElem.innerHTML = "<div class='alert alert-warning error_message'>*Please enter an Email*</div>";
+        fadeIn();
+        return false;
+    }
+    if (subject === "") {
+        errorMsgElem.innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
+        fadeIn();
+        return false;
+    }
+    if (comments === "") {
+        errorMsgElem.innerHTML = "<div class='alert alert-warning error_message'>*Please enter Comments*</div>";
+        fadeIn();
         return false;
     }
 
-    function fadeIn() {
-        var fade = document.getElementById("error-msg");
-        var opacity = 0;
-        var intervalID = setInterval(function () {
-            if (opacity < 1) {
-                opacity = opacity + 0.5
-                fade.style.opacity = opacity;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                simpleMsgElem.innerHTML = this.responseText;
+                document.forms["myForm"].reset();
             } else {
-                clearInterval(intervalID);
+                errorMsgElem.innerHTML = "<div class='alert alert-danger error_message'>*An error occurred. Please try again later.*</div>";
+                fadeIn();
             }
-        }, 200);
-    }
-} catch (error) {
-    
+        }
+    };
+    xhttp.open("POST", "php/contact.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(
+        "name=" + encodeURIComponent(name) +
+        "&email=" + encodeURIComponent(email) +
+        "&subject=" + encodeURIComponent(subject) +
+        "&comments=" + encodeURIComponent(comments)
+    );
+
+    return false; // Prevent default form submission
+}
+
+function fadeIn() {
+    var fade = document.getElementById("error-msg");
+    var opacity = 0;
+    var intervalID = setInterval(function () {
+        if (opacity < 1) {
+            opacity += 0.5;
+            fade.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 200);
 }
